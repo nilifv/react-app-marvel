cadbf2e1483b755b45aff3447dafdd20d24e2c03
Виктор Филин <filinn14@gmail.com>
2023-01-14 18:31:30 +0300

    propTypes

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,5 +1,6 @@
 
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 import MarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -108,7 +109,7 @@ const View = ({char}) => {
                 {comics.length > 0 ? null : 'There is no comics with this character'}
                 {
                     comics.map((item, i) => {
-                        if ( i > 9) return
+                        if ( i > 9) {return}
                         return (
                             <li key={i} className="char__comics-item">
                                 {item.name}
@@ -119,6 +120,10 @@ const View = ({char}) => {
             </ul>
         </>
     )
+}
+
+CharInfo.propTypes = {
+    charId: PropTypes.number
 }
 
 export default CharInfo;
